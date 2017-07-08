767fedce4a0f352929c2bef2d46fd7269608d910
El Topo <eltopo@live.com>
2017-07-08 12:57:43 -0700

    add localoptions.h for termux

---
+++ localoptions.h
@@ -0,0 +1,9 @@
+#ifndef DROPBEAR_LOCALOPTIONS_H
+#define DROPBEAR_LOCALOPTIONS_H
+
+/* NOTE: this file is added for termux, addressing on "overloadable" issue */
+
+#define LTC_NO_PROTOTYPES
+
+#endif /* DROPBEAR_LOCALOPTIONS_H */
+
